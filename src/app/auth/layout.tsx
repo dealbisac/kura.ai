3b9a043dfd7abeb696fa6a89e03465f1f89ddceb
dealbisac <chand.dipendra19@gmail.com>
2024-06-02 23:39:12 +0200

    chore: Update authentication layout with logo image and AI assistant section

--- src/app/auth/layout.tsx
+++ src/app/auth/layout.tsx
@@ -4,21 +4,21 @@ import { redirect } from 'next/navigation'
 import React from 'react'
 
 type Props = {
-    children: React.ReactNode
+  children: React.ReactNode
 }
 
-const Layout = async ({children}: Props) => {
-    const user = await currentUser()
+const Layout = async ({ children }: Props) => {
+  const user = await currentUser()
 
-    if (user) redirect('/')
+  if (user) redirect('/')
 
   return (
-    <div className='h-screen flex w-full justify-center'>
-      <div className='w-[600px] ld:w-full flex flex-col items-start p-6'>
+    <div className="h-screen flex w-full justify-center">
+      <div className="w-[600px] ld:w-full flex flex-col items-start p-6">
         <Image
-          src='/images/logo.png'
-          alt='Logo'
-          sizes='100 vw'
+          src="/images/logo.png"
+          alt="LOGO"
+          sizes="100vw"
           style={{
             width: '20%',
             height: 'auto',
@@ -27,6 +27,25 @@ const Layout = async ({children}: Props) => {
           height={0}
         />
         {children}
+      </div>
+      <div className="hidden lg:flex flex-1 w-full max-h-full max-w-4000px overflow-hidden relative bg-cream  flex-col pt-10 pl-24 gap-3">
+        <h2 className="text-gravel md:text-4xl font-bold">
+          Hi, I’m your AI powered sales assistant, Kura!
+        </h2>
+        <p className="text-iridium md:text-sm mb-10">
+          Kura is capable of capturing lead information without a form...{' '}
+          <br />
+          something never done before 😉
+        </p>
+        <Image
+          src="/images/app-ui.png"
+          alt="app image"
+          loading="lazy"
+          sizes="30"
+          className="absolute shrink-0 !w-[1600px] top-48"
+          width={0}
+          height={0}
+        />
       </div>
     </div>
   )
