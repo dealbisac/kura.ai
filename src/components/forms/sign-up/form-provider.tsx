95e3f0ea2436b54cf94148eee03fd18b4aab4464
dealbisac <chand.dipendra19@gmail.com>
2024-06-03 10:34:32 +0200

    feat: Add SignupFormProvider component for sign-up form

---
+++ src/components/forms/sign-up/form-provider.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+type Props = {
+    children: React.ReactNode
+}
+
+const SignupFormProvider = ({children}: Props) => {
+  return (
+    <div>SignUpFormProvider</div>
+  )
+}
+
+export default SignupFormProvider
