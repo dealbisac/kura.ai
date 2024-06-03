99bd882463cb088ad8b12666b336e3081a00aebd
dealbisac <chand.dipendra19@gmail.com>
2024-06-03 21:29:04 +0200

    feat: Update sign-up page with heading

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -8,7 +8,7 @@ const SignUp = (props: Props) => {
     <div className='flex-1 py-36 md:px-16 w-full'>
       <div className="flex flex-col h-full gap-3">
         <SignupFormProvider>
-          
+          <h1 className="text-4xl font-bold">Sign up</h1>
         </SignupFormProvider>
       </div>
     </div>
