31a7983a363183810b541712bf9722be596ae52b
dealbisac <chand.dipendra19@gmail.com>
2024-06-03 10:34:38 +0200

    feat: Update sign-up page with SignupFormProvider component

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,10 +1,17 @@
+import SignupFormProvider from '@/components/forms/sign-up/form-provider'
 import React from 'react'
 
 type Props = {}
 
 const SignUp = (props: Props) => {
   return (
-    <div>SignUp</div>
+    <div className='flex-1 py-36 md:px-16 w-full'>
+      <div className="flex flex-col h-full gap-3">
+        <SignupFormProvider>
+          
+        </SignupFormProvider>
+      </div>
+    </div>
   )
 }
 
