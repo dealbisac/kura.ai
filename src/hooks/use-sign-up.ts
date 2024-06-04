fc78ddac3e7e6c606a965f2ea763d4c2072e4a71
dealbisac <chand.dipendra19@gmail.com>
2024-06-04 11:16:03 +0200

    feat: Add useSignUpForm hook for sign-up form

---
+++ src/hooks/use-sign-up.ts
@@ -0,0 +1,7 @@
+import { useToast } from '@/components/ui/use-toast'
+import { useState } from 'react'
+
+export const useSignUpForm = () => {
+    const toast = useToast()
+    const [loading, setLoading] = useState()
+}
