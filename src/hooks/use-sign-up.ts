a90c1349aac84c56e3e8014b6801bdf1303eb426
dealbisac <chand.dipendra19@gmail.com>
2024-06-05 16:32:16 +0200

    feat: Add submission logic to useSignUpForm hook

--- src/hooks/use-sign-up.ts
+++ src/hooks/use-sign-up.ts
@@ -44,5 +44,58 @@ export const useSignUpForm = () => {
         }
       }
 
-
-}
+      // Logic on submission
+      const onHandleSubmit = methods.handleSubmit(
+        async (values: UserRegistrationProps) => {
+          if (!isLoaded) return
+    
+          try {
+            setLoading(true)
+            const completeSignUp = await signUp.attemptEmailAddressVerification({
+              code: values.otp,
+            })
+    
+            if (completeSignUp.status !== 'complete') {
+              return { message: 'Something went wrong!' }
+            }
+    
+            if (completeSignUp.status == 'complete') {
+              if (!signUp.createdUserId) return
+    
+              const registered = await onCompleteUserRegistration(
+                values.fullname,
+                signUp.createdUserId,
+                values.type
+              )
+    
+              if (registered?.status == 200 && registered.user) {
+                await setActive({
+                  session: completeSignUp.createdSessionId,
+                })
+    
+                setLoading(false)
+                router.push('/dashboard')
+              }
+    
+              if (registered?.status == 400) {
+                toast({
+                  title: 'Error',
+                  description: 'Something went wrong!',
+                })
+              }
+            }
+          } catch (error: any) {
+            toast({
+              title: 'Error',
+              description: error.errors[0].longMessage,
+            })
+          }
+        }
+      )
+      return {
+        methods,
+        onHandleSubmit,
+        onGenerateOTP,
+        loading,
+      }
+    }
