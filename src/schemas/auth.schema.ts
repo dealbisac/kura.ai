3800f35f53b5a99a511fa6653315bf309f75267f
dealbisac <chand.dipendra19@gmail.com>
2024-06-05 16:18:19 +0200

    feat: Add UserRegistrationPage schema for sign-up form

---
+++ src/schemas/auth.schema.ts
@@ -0,0 +1,9 @@
+export type UserRegistrationPage = {
+    type: string
+    fullname: string
+    email: string
+    confirmEmail: string
+    password: string
+    confirmPassword: string
+    otp: string
+}
