58d05bb0666ad168951f6ae4848892abd287621b
dealbisac <chand.dipendra19@gmail.com>
2024-06-06 00:29:40 +0200

    feat: Add RegistrationFormStep component for sign-up form

---
+++ src/components/forms/sign-up/registration-step.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const RegistrationFormStep = (props: Props) => {
+  return (
+    <div>RegistrationFormStep</div>
+  )
+}
+
+export default RegistrationFormStep
