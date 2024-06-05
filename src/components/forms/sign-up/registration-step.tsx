4398f6375db6096827700473e23c4235adc63511
dealbisac <chand.dipendra19@gmail.com>
2024-06-06 00:38:57 +0200

    refactor: Update RegistrationFormStep component for sign-up form

--- src/components/forms/sign-up/registration-step.tsx
+++ src/components/forms/sign-up/registration-step.tsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { useAuthContextHook } from '@/context/use-auth-context'
+import React, { useState } from 'react'
+import { useFormContext } from 'react-hook-form'
 
 type Props = {}
 
 const RegistrationFormStep = (props: Props) => {
+    const {
+        register,
+        formState: { errors },
+        setValue,
+      } = useFormContext()
+      const { currentStep } = useAuthContextHook()
+      const [onOTP, setOnOTP] = useState<string>('')
+      const [onUserType, setOnUserType] = useState<'owner' | 'student'>('owner')
+    
+      setValue('otp', onOTP)
+
+
   return (
     <div>RegistrationFormStep</div>
   )
