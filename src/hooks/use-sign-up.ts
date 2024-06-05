0c6d9e47d4c27e0e27a3bd5ac0d5b060ab9fb4fb
dealbisac <chand.dipendra19@gmail.com>
2024-06-05 16:25:22 +0200

    feat: Refactor useSignUpForm hook for sign-up form

--- src/hooks/use-sign-up.ts
+++ src/hooks/use-sign-up.ts
@@ -1,7 +1,18 @@
 import { useToast } from '@/components/ui/use-toast'
+import { UserRegistrationProps, UserRegistrationSchema } from '@/schemas/auth.schema'
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useClerk, useSignUp } from '@clerk/nextjs'
+import { useRouter } from 'next/navigation'
 import { useState } from 'react'
 
 export const useSignUpForm = () => {
-    const toast = useToast()
-    const [loading, setLoading] = useState()
+    const { toast } = useToast()
+    const [loading, setLoading] = useState<boolean(false)
+    const { signUp, isLoaded, setActive } = useSignUp()
+    const router = useRouter()
+    const methods = useForm(<UserRegistrationProps>({
+        resolver: zodResolver(UserRegistrationSchema),
+    }))
+
+
 }
