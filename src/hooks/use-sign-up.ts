ff1a4d1a64e77be68abafaae56a758dd8667c3f5
dealbisac <chand.dipendra19@gmail.com>
2024-06-05 16:31:09 +0200

    feat: Add OTP generation functionality to useSignUpForm hook

--- src/hooks/use-sign-up.ts
+++ src/hooks/use-sign-up.ts
@@ -18,4 +18,31 @@ export const useSignUpForm = () => {
         },
         mode: 'onChange',
     })
+
+    // function to handle on OTP generation
+    const onGenerateOTP = async (
+        email: string,
+        password: string,
+        onNext: React.Dispatch<React.SetStateAction<number>>
+      ) => {
+        if (!isLoaded) return
+    
+        try {
+          await signUp.create({
+            emailAddress: email,
+            password: password,
+          })
+    
+          await signUp.prepareEmailAddressVerification({ strategy: 'email_code' })
+    
+          onNext((prev) => prev + 1)
+        } catch (error: any) {
+          toast({
+            title: 'Error',
+            description: error.errors[0].longMessage,
+          })
+        }
+      }
+
+
 }
