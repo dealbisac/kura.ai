211f6db91a4c383afa702fcc5528df0f28c56a9f
dealbisac <chand.dipendra19@gmail.com>
2024-06-05 13:23:28 +0200

    Update title in layout.tsx to "Kura AI - Intelligent AI Chatbot."

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,7 +6,7 @@ import "./globals.css";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Kura.ai - Intelligent AI Chatbot.",
+  title: "Kura AI - Intelligent AI Chatbot.",
   description: "Automated AI sales chatbot.",
 };
 
