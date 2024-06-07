f4aad218ae2e0826be6e925b56bc8a231637dcc0
dealbisac <chand.dipendra19@gmail.com>
2024-06-07 14:32:39 +0200

    refactor: Restructure sign-up page components and add lazy loading for AccountDetailsForm and OTPForm

--- src/app/auth/sign-up/page.tsx
+++ src/app/auth/sign-up/page.tsx
@@ -1,15 +1,23 @@
-import SignupFormProvider from '@/components/forms/sign-up/form-provider'
+//import ButtonHandler from '@/components/forms/sign-up/button-handlers'
+import SignUpFormProvider from '@/components/forms/sign-up/form-provider'
+import HighLightBar from '@/components/forms/sign-up/hightlight-bar'
+import RegistrationFormStep from '@/components/forms/sign-up/registration-step'
+
 import React from 'react'
 
 type Props = {}
 
 const SignUp = (props: Props) => {
   return (
-    <div className='flex-1 py-36 md:px-16 w-full'>
+    <div className="flex-1 py-36 md:px-16 w-full">
       <div className="flex flex-col h-full gap-3">
-        <SignupFormProvider>
-          <h1 className="text-4xl font-bold">Sign up</h1>
-        </SignupFormProvider>
+      <SignUpFormProvider>
+          <div className="flex flex-col gap-3">
+            <RegistrationFormStep />
+            <ButtonHandler />
+          </div>
+          <HighLightBar />
+        </SignUpFormProvider>
       </div>
     </div>
   )
